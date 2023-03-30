1c3f4f1dcb67b75c16f66b175c8edf00d4dbe105
DELL <yetimaly@gmail.com>
2023-03-30 20:50:48 +0200

    Zadanie domowe  4

--- ChellangeApp/ChellangeApp/Program.cs
+++ ChellangeApp/ChellangeApp/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Helo Pawel Jest tutaj");
+string name = "Ewa";
+string sex = "Woman";
+int age = 33;
+
+if (sex == "Woman" && age < 30)
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+else if (name = "Ewa" && age == "33")
+{
+    Console.WriteLine("Ewa ma 33 lata");
+}
+else if (sex == "Man" && age < 18)
+{
+    Console.WriteLine("Niepelnoletni Mezczyzna");
+}
+{
+    Console.ReadKey();
+}
+{
+    Console.ReadLine();
+}
